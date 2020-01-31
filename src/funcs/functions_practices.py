e83c2bac6b3488ccd6c911996ec6024217262759
Subhashis <sarangi.subhashis@gmail.com>
2020-01-31 16:09:56 +0530

    func

---
+++ src/funcs/functions_practices.py
@@ -0,0 +1,12 @@
+'''
+Created on 30-Jan-2020
+
+@author: SUBHASHIS
+'''
+
+def fahrenheit(self,T_in_celsius):
+    """ returns the temperature in degrees Fahrenheit"""
+    return (T_in_celsius * 9 / 5) + 32
+
+for t in (22.6, 25.8, 27.3, 29.8):
+    print(t,":")  
